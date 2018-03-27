b80456ec389f24df1b7379464fbc7668a4262ce4
souvikbagchi <souvik.bagchi@gmail.com>
2018-03-28 01:11:08 +0530

    third commit

--- README.md
+++ README.md
@@ -3,10 +3,20 @@ This repo contains python class - findingS
 This class can be used to train a concept learning algorithm called Find S.
 The class is initialized by mentioning the following variables -
 positive_outcome - the value of the positive outcome in the result_set (eg. 'true', 1 etc.)
+no_of_attributes - the number of attributes in the training set
 result_list - a list of results, i.e. are they positive or negative
 attribute_list - a list of attributes for the corresponding results
 
-This class contains a method trainS which when called on an instantiated class variable will find the hypothesis
-according to the Find-S algorithm and store it in the instance variable hypothesis.
+Methods in this class - 
+1) trainS - 
+Parameters : None
+What it does : Will find the hypothesis according to the Find-S algorithm and store it in the instance variable hypothesis.
+What it Returns : Nothing
 
-An example of use is already provided at the end of the class
+2) resultS - 
+Parameters - Attributes to be checked against
+What it does - Classifies into positive or negative outcome
+What is Returns - 0 for negative outcome, 1 for positive classification
+
+
+An example of use is provided at the end of the class
--- findingS.py
+++ findingS.py
@@ -1,24 +1,25 @@
 class FindingS():
 
 	#INITIALIZE THE CLASS WITH THE INPUT OF OUTCOME AND TRAINING ATTRIBUTE LIST
-	def __init__(self,positive_outcome,result_list,attribute_list):
+	def __init__(self,positive_outcome,no_of_attributes,result_list,attribute_list):
 		#FIND OUT WHAT IS LISTED AS AN POSITIVE OUTCOME
 		self.positive_outcome = positive_outcome
 		#INITIALIZE THE RESULT LIST, ATTRIBUTE LIST AND THE LENGTH OF THE DATASET TO BE TRAINED UPON
 		self.result_list = result_list
 		self.attribute_list = attribute_list
-		self.len = len(result_list)
+		self.training_set_len = len(result_list)
+		self.attribute_len = no_of_attributes
 		#INITIALIZE EMPTY HYPOTHESIS LIST
 		self.hypothesis = []
 
 	#CREATE THE TRAINING METHOD
 	def trainS(self):
 		#INITIALIZE THE MOST SPECIFIC HYPOTHESIS
-		for i in range(0,self.len):
+		for i in range(0,self.attribute_len):
 			self.hypothesis.append(0)
 		#START TRAINING
 		#LOOP THROUGH THE TRAINING 
-		for x in xrange(0,self.len):
+		for x in xrange(0,self.training_set_len):
 			if self.result_list[x] == self.positive_outcome :
 				#ITERATE FOR ALL THE ATTRIBUTE
 				for attr in xrange(0,len(self.hypothesis)):
@@ -36,6 +37,22 @@ class FindingS():
 			else :
 				continue
 
+	def resultS(self,attributes):
+		#COMPARE THE ATTRIBUTES AND FIND THE RESULT
+		for i in range(0,self.attribute_len):
+			#COMPARE ALL THE ATTRIBUTES 
+			#CHECH IF THE HYPOTHESIS IS GENERIC - "?"
+			if self.hypothesis[i] == "?":
+				continue
+			else :
+				if self.hypothesis[i] != attributes[i]:
+					return(0)
+
+		return(1)
+
+
+
+
 #Example
-s = FindingS(1,[1,0,1],[[4,5,6],[2,7,4],[9,6,4]])
+s = FindingS(1,3,[1,0,1],[[4,5,6],[2,7,4],[9,6,4]])
 s.trainS()
